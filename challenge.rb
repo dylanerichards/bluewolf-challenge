c23980caa764c451558a218a08466f999a4d2ebc
Dylan Richards <dylanerichards@gmail.com>
2015-06-09 14:04:26 -0400

    Find first non-repeating chararacter in a string

--- challenge.rb
+++ challenge.rb
@@ -2,4 +2,11 @@ class Bluewolf
   def find_duplicate(array)
     array.detect { |e| array.count(e) > 1 }
   end
+
+  def first_non_repeating(string)
+    letters = string.split('')
+    duplicates = letters.find_all { |letter| letters.count(letter) > 1 }.uniq
+    non_duplicates = letters - duplicates
+    first_non_duplicate = non_duplicates[0]
+  end
 end
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -6,8 +6,15 @@ describe Bluewolf do
   describe "#find_duplicate" do
     it "finds the duplicate in the array" do
       expect(bluewolf.find_duplicate([1, 2, 3, 4, 5, 4, 6, 7, 8, 9, 10])).to eq 4
-
       expect(bluewolf.find_duplicate([1000000, 2, 3, 4, 5, 4, 6, 7, 8, 9, 1000000])).to eq 1000000
+    end
+  end
+
+  describe "#first_non_repeating" do
+    it 'finds the first onn-repeating character in a string' do
+      expect(bluewolf.first_non_repeating("defd")).to eq "e"
+      expect(bluewolf.first_non_repeating("bamboo")).to eq "a"
+      expect(bluewolf.first_non_repeating("coolness")).to eq "c"
     end
   end
 end
