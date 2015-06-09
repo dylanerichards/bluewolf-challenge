e24efc405e25c8fe80364d4fbf62dee23a0d41f6
Dylan Richards <dylanerichards@gmail.com>
2015-06-09 13:52:12 -0400

    Test with large number

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -6,6 +6,8 @@ describe Bluewolf do
   describe "#find_duplicate" do
     it "finds the duplicate in the array" do
       expect(bluewolf.find_duplicate([1, 2, 3, 4, 5, 4, 6, 7, 8, 9, 10])).to eq 4
+
+      expect(bluewolf.find_duplicate([1000000, 2, 3, 4, 5, 4, 6, 7, 8, 9, 1000000])).to eq 1000000
     end
   end
 end
