4396d71dbcf268b29b559b9975529eb68d8a001e
Dylan Richards <dylanerichards@gmail.com>
2015-06-09 14:05:12 -0400

    Be more expressive

--- challenge.rb
+++ challenge.rb
@@ -1,12 +1,12 @@
 class Bluewolf
   def find_duplicate(array)
-    array.detect { |e| array.count(e) > 1 }
+    array.detect { |element| array.count(element) > 1 }
   end
 
   def first_non_repeating(string)
     letters = string.split('')
     duplicates = letters.find_all { |letter| letters.count(letter) > 1 }.uniq
     non_duplicates = letters - duplicates
-    first_non_duplicate = non_duplicates[0]
+    first_non_duplicate = non_duplicates.first
   end
 end
